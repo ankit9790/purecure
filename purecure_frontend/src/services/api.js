008fd02837209166de0179e0689560975a98f33d
ankit <ankity9790@gmail.com>
2025-11-30 15:14:05 +0530

    same here

--- purecure_frontend/src/services/api.js
+++ purecure_frontend/src/services/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const API_URL = process.env.REACT_APP_BACKEND_URL;
+const API_URL = `${process.env.REACT_APP_BACKEND_URL}/customers`;
+
 
 // const API_URL = "http://localhost:3000/customers";
 
